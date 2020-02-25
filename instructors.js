82e7b8717d3ba789aa4b10f44f5e92cd8a4e0e70
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-02-24 22:06:03 -0300

    Feature: Data parse to timestamp, FileSystem JSON formatted and created an array to control and save the new input data at JSON file

--- instructors.js
+++ instructors.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const data = require('./data.json')
 
 //post
 exports.post = function(req, res){
@@ -12,8 +13,12 @@ exports.post = function(req, res){
             return res.send("Please, fill all the filds")
         }
     }
+    req.body.birth = Date.parse(req.body.birth)
+    req.body.created_at = Date.now();
 
-    fs.writeFile("data.json", JSON.stringify(req.body), function(err){
+    data.instructors.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if (err) throw err
         return res.redirect("/instructors")
     })
