e3333176c238b497665c4f4d47f6a74c1ddda354
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-02-29 11:00:19 -0300

    created button that leads to create page and bold apply on the link menu text

--- public/script.js
+++ public/script.js
@@ -1,3 +1,12 @@
 const currentPage = location.pathname
+const menuItens = document.querySelectorAll('header .links a')
 
-console.log(currentPage)
+
+for (item of menuItens){
+    if(currentPage.includes(item.getAttribute('href'))) {
+        item.classList.add("active")
+    }
+}
+
+
+
