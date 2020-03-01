fb6ec34ae3e3a8b5f45bb1a806cdb3ea5b4a5d89
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-03-01 17:51:36 -0300

    Created a model for db and instructor insert is working

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,14 +1,18 @@
+const Instructor = require('../models/Instructor.js')
 const { ageCalc, formatCreatedAt, dateFormat } = require('../../lib/utils')
-const db = require('../../config/db')
+
 module.exports = {
     index(req, res){
-        return res.render("instructors/index")
+        Instructor.all(function(instructors){
+            return res.render("instructors/index", { instructors })
+        })
+        
     },
     create(req, res){
         return res.render("instructors/create")
     },
     post(req, res){
-        const created_at = Date()
+        
         //req.query GET
         //req.body Post
 
@@ -24,38 +28,14 @@ module.exports = {
         
 
         //Treatment
-        const query = `
-            INSERT INTO instructors(
-                name, 
-                avatar_url,
-                gender,
-                services,
-                birth,
-                created_at
-            ) values($1,$2,$3,$4,$5,$6)
-            RETURNING id
-        `
-        const { name, avatar_url, gender, services, birth} = req.body
-        const values =[
-            name,
-            avatar_url,
-            gender,
-            services,
-            dateFormat(birth).iso,
-            dateFormat(created_at).iso
-        ]
-
-
-        db.query(query,values, function(err, results){
-            if(err) return res.send(err)
-            console.log(results)
-            return res.redirect(`/instructors/${results.rows[0].id}`)
+        Instructor.create(req.body, function(instructor){
+            return res.redirect(`/instructors/${instructor}`)
         })
 
         
     },
     show(req, res){
-        return
+        return res.send('Working')
     },
     edit(req, res){
         return
---
+++ src/app/models/Instructor.js
@@ -0,0 +1,42 @@
+const db = require('../../config/db')
+const { ageCalc, formatCreatedAt, dateFormat } = require('../../lib/utils')
+
+module.exports ={
+    all(callback){
+        db.query(`SELECT * FROM instructors`, function(err, results){
+            if(err) res.send(err)
+            callback(results.rows)
+        })
+
+    },
+    create(reqBody, callback){
+        const query = `
+            INSERT INTO instructors(
+                name, 
+                avatar_url,
+                gender,
+                services,
+                birth,
+                created_at
+            ) values($1,$2,$3,$4,$5,$6)
+            RETURNING id
+        `
+        const { name, avatar_url, gender, services, birth} = reqBody
+        const values =[
+            name,
+            avatar_url,
+            gender,
+            services,
+            dateFormat(birth).iso,
+            dateFormat(Date.now()).iso
+        ]
+        
+
+        db.query(query,values, function(err, results){
+            if(err) return res.send(err)
+            callback(results.rows[0].id)
+        })
+    },
+
+
+}
